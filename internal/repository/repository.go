ecad46298287831a703a179a8bf71f3eaa7beca7
Firuz Chamarov <firuzchamarov@gmail.com>
2026-09-20 11:03:08 +0500

    repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,4 +11,7 @@ type OrderRepository interface {
 	// CreateOrder creates a new order in the database
 	Create(ctx context.Context, order *models.Order) error
 	GetByID(ctx context.Context, id uuid.UUID) (*models.Order, error)
+	List(ctx context.Context, limit, offset int)([]*models.Order, error)
+	Update(ctx context.Context, order *models.Order)error
+	Delete(ctx context.Context, id uuid.UUID) error
 }
