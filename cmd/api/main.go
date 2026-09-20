93985e1be3dbdb2ac969edc680551da409de1995
Firuz Chamarov <firuzchamarov@gmail.com>
2026-09-20 08:40:13 +0500

    database docker postgresql successful

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,27 @@
-package api
+package main
+
+import (
+	"log"
+
+	"github.com/Firuz43/order-service/internal/config"
+	"github.com/Firuz43/order-service/internal/db"
+)
+
+func main() {
+	// 1. Load configuration
+	cfg := config.LoadConfig()
+
+	// 2. Conenct to PostgreSQL
+	dbpool, err := db.ConnectPostgres(cfg)
+
+	if err != nil {
+		log.Fatalf("Fatal: Could not initialize database %v", err)
+	}
+
+	// Clean up pool when main function exists
+	defer dbpool.Close()
+
+	log.Printf("Server starting on port %s", cfg.ServerPort)
+
+	// Next: We will set up HTTP server router & route here
+}
