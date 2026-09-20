012e2e80b5776403193fda5b635b18789bcaa0be
Firuz Chamarov <firuzchamarov@gmail.com>
2026-09-20 09:19:40 +0500

    models order.go

---
+++ internal/models/order.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"time"
+	"uuid"
+)
+
+type OrderStatus string
+
+const (
+	StatusPending   OrderStatus = "PENDING"
+	StatusCompleted OrderStatus = "COMPLETED"
+	StatusCancelled OrderStatus = "CANCELLED"
+)
+
+type Order struct {
+	ID           uuid.UUID   `json:"id"`
+	CustomerName string      `json:"customer_name"`
+	Item         string      `json:"item"`
+	Quantity     int         `json:"quantity"`
+	Price        float64     `json:"price"`
+	Status       OrderStatus `json:"status"`
+	CreatedAt    time.Time   `json:"created_at"`
+	UpdatedAt    time.Time   `json:"updated_at"`
+}
+
+// Payload expected from HTTP client
+type CreateOrderRequest struct {
+	CustomerName string  `json:"customer_name"`
+	Item         string  `json:"item"`
+	Quantity     int     `json:"quantity"`
+	Price        float64 `json: "price"`
+}
