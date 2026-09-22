4af46f7441b1a9ec4ef07a8320ab0acd9272bee9
Firuz Chamarov <firuzchamarov@gmail.com>
2026-09-22 16:07:00 +0500

    order_handler CreateOrder function

--- internal/handlers/order_handler.go
+++ internal/handlers/order_handler.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
+	"github.com/Firuz43/order-service/internal/models"
 	"github.com/Firuz43/order-service/internal/service"
 )
 
@@ -29,8 +31,30 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 // JSON Error Helper Function
-func responsdWithError(w http.ResponseWriter, code int, message string) {
+func respondWithError(w http.ResponseWriter, code int, message string) {
 	respondWithJSON(w, code, map[string]string{"error": message})
 }
 
-//
+// POST /orders
+func (h *OrderHandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
+	var req models.CreateOrderRequest
+
+	// Decode JSON Body
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid JSON payload")
+		return
+	}
+
+	// Call Service
+	order, err := h.svc.CreateOrder(r.Context(), &req)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidInput) {
+			respondWithError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		respondWithError(w, http.StatusInternalServerError, "Failed to create order")
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, order)
+}
